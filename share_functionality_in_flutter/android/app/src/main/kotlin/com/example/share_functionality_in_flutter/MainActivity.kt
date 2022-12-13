66d8091d71ea727cabfc733efb375dc73b9f0e5c
unknown <dinkar3636@gmail.com>
2022-12-13 17:45:24 +0530

    first

---
+++ share_functionality_in_flutter/android/app/src/main/kotlin/com/example/share_functionality_in_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.share_functionality_in_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ share_functionality_in_flutter/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Share Functionality'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body:
+          Container(
+            child: Center(
+              child: ElevatedButton(
+                onPressed: (){
+                  Share.share("Hi I m share this text using flutter code", subject: 'Look what I made!');
+                },
+                child: Text("Share Text with other apps"),
+              ),
+            ),
+          ),
+    );
+  }
+}
