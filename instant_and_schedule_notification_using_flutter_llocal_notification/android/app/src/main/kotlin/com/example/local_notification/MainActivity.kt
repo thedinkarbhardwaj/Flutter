71b172360c1795063f24fd595738a17cae4285c3
unknown <dinkar3636@gmail.com>
2022-12-14 11:28:38 +0530

    first

---
+++ instant_and_schedule_notification_using_flutter_llocal_notification/android/app/src/main/kotlin/com/example/local_notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instant_and_schedule_notification_using_flutter_local_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ instant_and_schedule_notification_using_flutter_llocal_notification/lib/notification_service.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationServices {
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  //  Specific setting for Platform like android
+  
+AndroidInitializationSettings _androidInitializationSettings = AndroidInitializationSettings('rain');
+
+ void initializseNotification() async{
+   InitializationSettings _initializesetting = InitializationSettings(
+     android: _androidInitializationSettings
+   );
+
+   await flutterLocalNotificationsPlugin.initialize(_initializesetting);
+ }
+
+  void sendnotification(String title,String body) async{
+
+   AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails(
+       "channelId",
+       "channelName",
+   priority: Priority.high);
+
+   NotificationDetails notificationDetails = NotificationDetails(
+     android: androidNotificationDetails
+   );
+
+   flutterLocalNotificationsPlugin.show(
+       0,
+       title,
+       body,
+       notificationDetails);
+
+  }
+
+
+  // Schedule notification means set notification after giving time
+  void Schedule_sendnotification(String title,String body) async{
+
+    AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails(
+        "channelId",
+        "channelName",
+        priority: Priority.high);
+
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails
+    );
+
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+        0,
+        title,
+        body,
+        RepeatInterval.everyMinute, // you can also use hourly,day,weekly
+        notificationDetails
+    );
+
+} }
